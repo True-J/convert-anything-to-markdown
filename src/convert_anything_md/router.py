82667aae6b7f626290bf40f4e15818f1bfb42a45
True-J <truesdale.jacob@gmail.com>
2026-08-07 20:37:15 -0500

    Router.py import issue fixed

--- src/convert_anything_md/router.py
+++ src/convert_anything_md/router.py
@@ -50,6 +50,7 @@ from convert_anything_md.extractors.text import (
     RtfPandocExtractor,
     RtfStripExtractor,
 )
+from convert_anything_md.extractors.vcf import VCFExtractor
 from convert_anything_md.frontmatter import build_frontmatter
 from convert_anything_md.paths import (
     conflict_safe_name,
@@ -57,7 +58,6 @@ from convert_anything_md.paths import (
     ensure_dir,
     safe_stem,
 )
-from convert_anything_md.extractors.vcf import VCFExtractor
 
 # ---------------------------------------------------------------------------
 # Extractor chains
